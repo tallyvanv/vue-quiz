b81a357fecea3952fedee152fdfacb4bb15b1906
tallyvanv <talisa.vdv@gmail.com>
2020-08-27 10:01:37 +0200

    first push vue quiz

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import QuestionBox from './components/QuestionBox.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    QuestionBox
+  },
+  mounted: function() {
+    fetch('https://opentdb.com/api.php?amount=10&category=23&type=multiple', {
+      method: 'get'
+    })
+    .then((response) => {
+      console.log(response.json())
+    })
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <b-nav tabs>
+            <b-nav-item disabled><strong>Hoeveel populairder</strong></b-nav-item>
+            <b-nav-item disabled>Counter: 4/10</b-nav-item>
+        </b-nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
